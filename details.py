8a27671208cddadfd3816e5c7db824c3850a1e9b
thabiso041130 <thabzen3011@gmail.com>
2026-02-04 15:04:32 +0200

    Add files via upload

---
+++ details.py
@@ -0,0 +1,6 @@
+# Use input command to get information from the user
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+house_number = input("Enter your house number: ")
+street_name = input("Enter your street name: ")
+print("My name is {} and I am {} years old. I live at house number {} on {}. ".format(name, age, house_number, street_name))
